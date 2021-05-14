bbc9d0e8ea80cc4bc517fa7c7dbd906c447ff469
jhal32 <jack.hall@nike.com>
2021-05-13 21:26:33 -0500

    extra credit 4 Flexible localStorage

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,20 +2,38 @@
 // http://localhost:3000/isolated/exercise/02.js
 
 import * as React from 'react'
-function useSyncLocalStorage(key, defaultValue = "") {
+function useSyncLocalStorage(
+  key,
+  defaultValue = "",
+  {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+  ) {
    // 🐨 initialize the state to the value from localStorage
    // eslint-disable-next-line react-hooks/rules-of-hooks
-   const [state, setState] = React.useState(
-    () => window.localStorage.getItem(key) || defaultValue 
-    )
+   const [state, setState] = React.useState(() => {
+    const valueInLocalStorage = window.localStorage.getItem(key)
+    if (valueInLocalStorage) {
+       return deserialize(valueInLocalStorage);
+    }
+    return typeof defaultValue === "function" ? defaultValue() : defaultValue;
+   });
+    
+   // what happens if key changes value
+   const previousKeyRef = React.useRef(key);
+   // this doesn't trigger a rerender
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
   // eslint-disable-next-line react-hooks/rules-of-hooks
   React.useEffect(() => {
-    window.localStorage.setItem(key, state);
-  }, [key, state]);
+    const prevKek = previousKeyRef.current
+    if (prevKek !== key) {
+      window.localStorage.remove(previousKeyRef)
+    }
+
+    previousKeyRef.current = key
+    window.localStorage.setItem(key, serialize(state));
+  }, [key, serialize,state]);
     return [ state, setState]
 }
 
