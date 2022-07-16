cdab86499026607a01f1862527ce2393d6251225
Jack Hall <jackhall@Jacks-Mac-mini.local>
2022-07-16 10:57:37 -0500

    complete basic requirements

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,6 +2,7 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
+import { fetchPokemon, PokemonInfoFallback, PokemonDataView } from '../pokemon'
 // 🐨 you'll want the following additional things from '../pokemon':
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
@@ -9,6 +10,19 @@ import * as React from 'react'
 import {PokemonForm} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
+  const [pokemon, setPokemon] = React.useState(null)
+
+  React.useEffect(() => {
+    if(!pokemonName) return
+    setPokemon(null)
+    fetchPokemon(pokemonName).then((data) => {
+      setPokemon(data)
+    })
+  }, [pokemonName])
+
+  if (pokemonName === null) return 'Submit a pokemon'
+  if (!pokemonName) return <PokemonInfoFallback name={pokemonName} />
+  if (pokemon) return <PokemonDataView pokemon={pokemon} />
   // 🐨 Have state for the pokemon (null)
   // 🐨 use React.useEffect where the callback should be called whenever the
   // pokemon name changes.
@@ -26,7 +40,6 @@ function PokemonInfo({pokemonName}) {
   //   3. pokemon: <PokemonDataView pokemon={pokemon} />
 
   // 💣 remove this
-  return 'TODO'
 }
 
 function App() {
